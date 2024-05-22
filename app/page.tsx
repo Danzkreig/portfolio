ee69fc62de207b438a92f806fe8e8b8762cf4bc9
tuvshinm <m.tuvshin@proton.me>
2024-05-22 15:03:16 +0800

    Initial Commit.

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import MainGreeting from "@/components/mainGreeting";
+
+export default function Index() {
+  return (
+    <>
+      <MainGreeting />
+    </>
+  );
+}
---
+++ components/mainGreeting/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+export default function MainGreeting() {
+  return (
+    <div className="flex items-center justify-center h-screen flex-col">
+      <div>
+        <h1 className="text-4xl font-bold text-white">
+          Hello, My name is Tuvshin Munkh-Ireedui.
+        </h1>
+        <h1 className="text-3xl font-semibold text-white">
+          I am a full stack developer.
+        </h1>
+        <h3></h3>
+      </div>
+    </div>
+  );
+}
